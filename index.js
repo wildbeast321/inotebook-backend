93721adc07ccc5761486eb3f917cf76850adabae
Anirudh Acharya <anirudhacharya2001@gmail.com>
2022-07-14 21:09:24 +0530

    minor changes

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use("/api/notes",require("./routes/notes"))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`iNotebook- Backend listening on port http://localhost:${port}`)
 })
