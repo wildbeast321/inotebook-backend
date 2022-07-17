11f0e0a163cad75bea4a1989f3357377da1b667b
Anirudh Acharya <anirudhacharya2001@gmail.com>
2022-07-17 14:39:43 +0530

    Minor changes

--- Models/Note.js
+++ Models/Note.js
@@ -4,7 +4,7 @@ const notesSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  tag: { type: String, default: "General" },
+  tag: { type: String, default: "Personal" },
   date: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("notes", notesSchema);
